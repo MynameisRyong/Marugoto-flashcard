61d9012520c1dfc44f0b2a21896ff32270823e4f
MynameisRyong <111057332+MynameisRyong@users.noreply.github.com>
2025-12-07 12:24:18 +0700

    Add files via upload
    Update PWA + UI + Flashcard changes

--- index.tsx
+++ index.tsx
@@ -13,3 +13,13 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .catch(err => {
+        console.warn("Service worker registration failed", err);
+      });
+  });
+}
